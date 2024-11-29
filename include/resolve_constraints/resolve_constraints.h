0f76048284c9fe3fe3818fffc0b183b662ac04ee
Manya Bansal <manya227@mit.edu>
2024-11-29 12:21:40 -0500

    minor

--- include/resolve_constraints/resolve_constraints.h
+++ include/resolve_constraints/resolve_constraints.h
@@ -9,21 +9,21 @@ namespace gern {
 namespace resolve {
 
 /**
- * @brief Given a system of equations, return the solution for 
+ * @brief Given a system of equations, return the solution for
  *        each Variable.
- * 
- * Currently, solve uses the SymEngine library. To plug-in a different
- * method for bounds inference, re-implement this function adhering to 
- * same interface. There are no dependencies on SymEngine except in the 
- * implementation of this function. 
- * 
+ *
+ * Currently, solve uses the GiNaC library. To plug-in a different
+ * method for bounds inference, re-implement this function adhering to
+ * same interface. There are no dependencies on GiNaC except in the
+ * implementation of this function.
+ *
  * @param system_of_equations The system of equations
  * @return std::map<Variable, Expr> Map from system of equations to
- *                                  solutions. 
+ *                                  solutions.
  */
 std::map<Variable, Expr> solve(std::vector<Expr> system_of_equations);
 
-}
+} // namespace resolve
 } // namespace gern
 
 #endif
--- src/resolve_constraints/resolve_constraints.cpp
+++ src/resolve_constraints/resolve_constraints.cpp
@@ -8,21 +8,33 @@
 namespace gern {
 namespace resolve {
 
-// #define VISIT_AND_DECLARE(op, operation)                                       \
-//   void visit(const op *node) {                                                 \
-//     node->a.accept(this);                                                      \
-//     auto a = expr;                                                             \
-//     node->b.accept(this);                                                      \
-//     expr = operation(a, expr);                                                 \
-//   }
+#define VISIT_AND_DECLARE(op, operation)                                       \
+  void visit(const op *node) {                                                 \
+    node->a.accept(this);                                                      \
+    auto a = expr;                                                             \
+    node->b.accept(this);                                                      \
+    expr = operation(a, expr);                                                 \
+  }
 
-// #define COMPLAIN(op)                                                           \
-//   void visit(const op *node) { assert("bad"); }
+#define COMPLAIN(op)                                                           \
+  void visit(const op *node) { assert("bad"); }
 
+typedef std::map<const VariableNode *, GiNaC::symbol> SymbolVariableMap;
+
+GiNaC::ex convertToGinac() {}
 
 std::map<Variable, Expr> solve(std::vector<Expr> system_of_equations) {
-  // Generate a SymEngine symbol for each variable node that we want to lower.
-  GiNaC::symbol t("x");
+  // Generate a GiNaC symbol for each variable node that we want to lower.
+  SymbolVariableMap symbols;
+
+  for (const auto &eq : system_of_equations) {
+    match(eq, std::function<void(const VariableNode *, Matcher *)>(
+                  [&](const VariableNode *op, Matcher *ctx) {
+                    symbols[op] = GiNaC::symbol(op->name);
+                  }));
+  }
+
+  GiNaC::symbol y("x");
   return {};
 }
 } // namespace resolve
