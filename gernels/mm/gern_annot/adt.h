74d56d0a378d1f201603a0914599c03336a38c90
Manya Bansal <manya227@mit.edu>
2025-03-04 16:05:43 -0500

    correct!

--- gernels/mm/gern_annot/adt.h
+++ gernels/mm/gern_annot/adt.h
@@ -1,24 +1,150 @@
 #pragma once
 
-#include <cstdlib>
-#include <cstring>
-#include <cuda/atomic>
-#include <cuda_runtime.h>
-#include <iostream>
-
 #include "annotations/abstract_function.h"
-#include "annotations/data_dependency_language.h"
-#include "compose/composable.h"
 
 using namespace gern;
 
 namespace annot {
 
+template<int Row, int Col, int Stride>
 class MatrixGPU : public AbstractDataType {
 public:
-    MatrixGPU(int64_t rows, int64_t cols)
-        : rows(rows), cols(cols) {
+    MatrixGPU(const std::string &name, bool temp)
+        : name(name), temp(temp) {
+    }
+    std::string getName() const override {
+        return name;
+    }
+    std::string getType() const override {
+        if (temp) {
+            return "auto";
+        } else {
+            return "impl::MatrixGPU<" +
+                   std::to_string(Row) + "," +
+                   std::to_string(Col) + "," +
+                   std::to_string(Col) + "," +
+                   std::to_string(Stride) +
+                   ">";
+        }
+    }
+
+    std::vector<Variable> getFields() const {
+        return {x, y, row, col};
+    }
+    FunctionSignature getAllocateFunction() const {
+        return FunctionSignature{
+            .name = "impl::allocate_static",
+            .template_args = {row, col},
+        };
+    }
+    FunctionSignature getFreeFunction() const {
+        return FunctionSignature{
+            .name = "destroy",
+        };
+    }
+    FunctionSignature getInsertFunction() const {
+        return FunctionSignature{
+            .name = "template insert",
+            .args = {x, y},
+        };
+    }
+    FunctionSignature getQueryFunction() const {
+        return FunctionSignature{
+            .name = "template query",
+            .args = {x, y},
+            .template_args = {row, col},
+        };
+    }
+    bool insertQuery() const override {
+        return true;
+    }
+    bool freeAlloc() const override {
+        return false;
+    }
+
+protected:
+    std::string name;
+    Variable x{"x"};
+    Variable y{"y"};
+    Variable row{"row", Datatype::Int64, true};
+    Variable col{"col", Datatype::Int64, true};
+    bool temp;
+};
+
+template<int Row, int Col, int Stride>
+class MatrixGPUSequential : public MatrixGPU<Row, Col, Stride> {
+public:
+    MatrixGPUSequential(const std::string &name, bool temp)
+        : MatrixGPU<Row, Col, Stride>(name, temp) {
+    }
+    FunctionSignature getQueryFunction() const {
+        return FunctionSignature{
+            .name = "template query",
+            .args = {this->x, this->y},
+            .template_args = {this->row, this->col},
+        };
+    }
+    FunctionSignature getInsertFunction() const {
+        return FunctionSignature{
+            .name = "template insert",
+            .args = {this->x, this->y},
+        };
     }
 };
 
-}  // namespace annot
+class StaticArray : public AbstractDataType {
+public:
+    StaticArray(const std::string &name,
+                bool temp = true)
+        : name(name), temp(temp) {
+    }
+
+    std::string getName() const override {
+        return name;
+    }
+
+    std::string getType() const override {
+        if (temp) {
+            return "auto";
+        }
+        return "impl::StaticArray";
+    }
+    std::vector<Variable> getFields() const {
+        return {x, len};
+    }
+
+    FunctionSignature getAllocateFunction() const override {
+        return FunctionSignature{
+            .name = "impl::allocate_static_array",
+            .template_args = {len},
+        };
+    }
+    FunctionSignature getFreeFunction() const override {
+        return FunctionSignature{
+            .name = "dummy",
+        };
+    }
+
+    FunctionSignature getInsertFunction() const override {
+        return FunctionSignature{
+            .name = "dummy",
+        };
+    }
+
+    FunctionSignature getQueryFunction() const override {
+        return FunctionSignature{
+            .name = "dummy",
+        };
+    }
+    bool freeAlloc() const override {
+        return false;
+    }
+
+private:
+    std::string name;
+    Variable x{"x"};
+    Variable len{"size", Datatype::Int64, true};
+    bool temp;
+};
+
+};  // namespace annot
---
+++ gernels/mm/gern_annot/functions.h
@@ -0,0 +1,82 @@
+#pragma once
+
+#include "adt.h"
+
+namespace annot {
+
+class MatrixMultiply : public AbstractFunction {
+public:
+    MatrixMultiply(AbstractDataTypePtr A,
+                   AbstractDataTypePtr B,
+                   AbstractDataTypePtr C)
+        : A(A), B(B), C(C) {
+        // k = k.bindToInt64(K);
+    }
+    Annotation getAnnotation() override {
+        Variable i("i");
+        Variable j("j");
+        Variable k("k");
+
+        Variable ti("ti", Datatype::Int64, true);
+        Variable tj("tj", Datatype::Int64, true);
+        Variable tk("tk", Datatype::Int64, true);
+
+        return For(i = Expr(0), ADTMember(C, "row", true), ti,
+                   For(j = Expr(0), ADTMember(C, "col", true), tj,
+                       Produces::Subset(C, {i, j, ti, tj}),
+                       Consumes::Subsets(
+                           Reduce(k = Expr(0), k_dim, tk,
+                                  SubsetObjMany({
+                                      SubsetObj(A, {i, k, ti, tk}),
+                                      SubsetObj(B, {k, j, tk, tj}),
+                                  })
+
+                                      ))))
+            .occupies({Grid::Unit::SCALAR_UNIT});
+    }
+    virtual FunctionSignature getFunction() override {
+        FunctionSignature f;
+        f.name = "matrix_multiply";
+        f.args = {
+            Parameter(A),
+            Parameter(B),
+            Parameter(C),
+        };
+        f.template_args = {k_dim};
+        return f;
+    }
+    std::vector<std::string> getHeader() override {
+        return {
+            "impl/matrix-gpu.h",
+            "impl/matrix_multiply.h",
+        };
+    }
+
+protected:
+    AbstractDataTypePtr A;
+    AbstractDataTypePtr B;
+    AbstractDataTypePtr C;
+    Variable k_dim{"k_dim", Datatype::Int64, true};
+};
+
+class MatrixMultiplyReg : public MatrixMultiply {
+public:
+    MatrixMultiplyReg(AbstractDataTypePtr A,
+                      AbstractDataTypePtr B,
+                      AbstractDataTypePtr C)
+        : MatrixMultiply(A, B, C) {
+    }
+    virtual FunctionSignature getFunction() override {
+        FunctionSignature f;
+        f.name = "matrix_multiply_reg";
+        f.args = {
+            Parameter(A),
+            Parameter(B),
+            Parameter(C),
+        };
+        f.template_args = {k_dim};
+        return f;
+    }
+};
+
+}  // namespace annot
