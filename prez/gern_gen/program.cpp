55150cf8d14d6dc77eb3641adfee3b864c7debde
Manya Bansal <manya227@mit.edu>
2025-03-10 10:23:40 -0400

    ----checkpoint-----

--- prez/gern_gen/program.cpp
+++ prez/gern_gen/program.cpp
@@ -1,16 +1,15 @@
 #include "cassert"
 #include "cpu-array.h"
 
-void function_3(library::impl::ArrayCPU &input,
-                library::impl::ArrayCPU &output) {
+void function_3(library::impl::ArrayCPU &a, library::impl::ArrayCPU &b) {
 
-    library::impl::add_1(input, output);
+    library::impl::add_1(a, b);
 }
 
 extern "C" {
 void hook_function_3(void **args) {
-    library::impl::ArrayCPU &input = *((library::impl::ArrayCPU *)args[0]);
-    library::impl::ArrayCPU &output = *((library::impl::ArrayCPU *)args[1]);
-    function_3(input, output);
+    library::impl::ArrayCPU &a = *((library::impl::ArrayCPU *)args[0]);
+    library::impl::ArrayCPU &b = *((library::impl::ArrayCPU *)args[1]);
+    function_3(a, b);
 }
 }
